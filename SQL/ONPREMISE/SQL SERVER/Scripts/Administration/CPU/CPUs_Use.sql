015b9c51285b729adc5dba98eff691de72d9c500
LucasLiberatori <45408340+LucasLiberatori@users.noreply.github.com>
2022-06-02 21:48:07 -0300

    New commit

--- SQL/ONPREMISE/SQL SERVER/Scripts/Administration/CPU/CPUs_Use.sql
+++ SQL/ONPREMISE/SQL SERVER/Scripts/Administration/CPU/CPUs_Use.sql
@@ -22,3 +22,50 @@ FROM (
         ) AS x
     ) AS y
 ORDER BY record_id DESC
+
+
+-- Script to check CPU Online
+SELECT
+	r.session_id
+	, st.text as Batch_Text
+	, SUBSTRING (st.text, statement_start_offset / 2 + 1, (
+					(
+						CASE
+							WHEN r.statement_end_offset = -1
+								THEN (LEN(CONVERT(NVARCHAR(MAX), st.text)) * 2)
+							ELSE
+								r.statement_end_offset
+						END
+					) - r.statement_start_offset
+				) / 2 + 1) AS Statement_Text
+	, qp.query_plan AS 'XML Plan'
+	, r.cpu_time
+	, r.total_elapsed_time
+	, r.logical_reads
+	, r.writes
+	, r.dop
+
+FROM SYS.DM_EXEC_REQUESTS R
+CROSS APPLY SYS.DM_EXEC_SQL_TEXT (r.sql_handle) AS ST
+CROSS APPLY SYS.DM_EXEC_QUERY_PLAN (r.plan_handle) AS QP
+ORDER BY CPU_TIME
+
+-- Script to checl CPU use in the past time
+SELECT
+	TOP 50
+	qs.execution_count AS [Execution Count]
+	, (qs.total_logical_reads) / 1000.0 AS [Total Logical Reads in ms]
+	, (qs.total_logical_reads / qs.execution_count) / 1000.0 AS [Avg Logical REads in ms]
+	, (qs.total_worker_time) / 1000.0 AS [Total Worker Time in ms]
+	, (qs.total_worker_time / qs.execution_count) / 1000.0 AS [Avg Worker time in ms]
+	, (qs.total_elapsed_time) / 1000.0 AS [Total Elapsed Time in ms]
+	, (qs.total_elapsed_time / qs.execution_count) / 1000.0 AS [Avg Elapsed Time in ms]
+	, qs.creation_time AS [Creation Time]
+	, t.text AS [Complete Query Text]
+	, qp.query_plan AS [Query Plan]
+
+FROM SYS.DM_EXEC_QUERY_STATS AS qs WITH(NOLOCK)
+CROSS APPLY SYS.DM_EXEC_SQL_TEXT(plan_handle) AS t
+CROSS APPLY SYS.DM_EXEC_QUERY_PLAN(plan_handle) AS qp
+-- WHERE t.dbid = DB_ID()
+ORDER BY (qs.total_logical_reads / qs.execution_count) DESC
